37a3abe6ea92ba9ee22ff4bfb10fda6664e37235
Tulya Jain <tulya.jain.cg@gmail.com>
2026-04-16 20:25:29 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 4/16/2026, 8:25:29 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return 'Hello World'
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
