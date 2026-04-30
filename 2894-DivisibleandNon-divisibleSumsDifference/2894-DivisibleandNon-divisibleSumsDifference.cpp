f86dda2cb12732596dcbcbe844a984f0503edb64
Tulya Jain <tulya.jain.cg@gmail.com>
2026-04-30 14:17:55 +0530

    Create 2894-DivisibleandNon-divisibleSumsDifference

---
+++ 2894-DivisibleandNon-divisibleSumsDifference/2894-DivisibleandNon-divisibleSumsDifference.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/30/2026, 2:17:56 PM
+1class Solution {
+2public:
+3    int differenceOfSums(int n, int m) {
+4        int num1=0;
+5        int num2=0;
+6        for(int i=1; i<=n ; i++){
+7            if(i%m!=0) num1+=i;
+8        
+9        if(i%m==0) num2+=i;}
+10        return num1-num2;
+11    }
+12    
+13};
