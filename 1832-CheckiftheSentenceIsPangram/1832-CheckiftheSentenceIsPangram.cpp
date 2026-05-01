a6963945123901cb765001b1ece6b2cbb89a9a99
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-01 16:57:04 +0530

    File updated at 5/1/2026, 4:57:04 PM

--- 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
+++ 1832-CheckiftheSentenceIsPangram/1832-CheckiftheSentenceIsPangram.cpp
@@ -1,7 +1,18 @@
-// Last updated: 4/30/2026, 1:02:26 PM
+// Last updated: 5/1/2026, 4:57:04 PM
 1class Solution {
 2public:
 3    bool checkIfPangram(string sentence) {
-4        return unordered_set<char>(sentence.begin(), sentence.end()).size() == 26;
-5    }
-6};
+4        vector<bool> check(26, false);
+5
+6        for (char ch : sentence) {
+7
+8            check[ch - 'a'] = true;
+9        }
+10        for (bool val : check) {
+11            if (!val)
+12                return false;
+13        }
+14
+15        return true;
+16    }
+17};
