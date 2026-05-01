cd97d25404a0a7063b7be14f830991eb4d24601e
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-01 17:32:15 +0530

    File updated at 5/1/2026, 5:32:15 PM

--- 1486-XOROperationinanArray/1486-XOROperationinanArray.cpp
+++ 1486-XOROperationinanArray/1486-XOROperationinanArray.cpp
@@ -1,13 +1,13 @@
-// Last updated: 4/30/2026, 2:06:06 PM
+// Last updated: 5/1/2026, 5:32:15 PM
 1class Solution {
 2public:
 3    int xorOperation(int n, int start) {
-4        int result = 0;
+4        int result=0;
 5
-6        for (int i = 0; i < n; i++) {
-7            result ^= (start + 2 * i);
-8        }
-9
+6        for(int i=0 ; i<n ;i++){
+7
+8            result^=(start+2*i);
+9        }
 10        return result;
 11    }
 12};
