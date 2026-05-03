b3d2d0ed9eb9b5c35136e94e6a4cab21ab766911
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 22:37:18 +0530

    Create 1610-XorOperationInAnArray

---
+++ 1610-XorOperationInAnArray/1610-XorOperationInAnArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/3/2026, 10:37:18 PM
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int result=0;
+
+        for(int i=0 ; i<n ;i++){
+
+            result^=(start+2*i);
+        }
+        return result;
+    }
+};
