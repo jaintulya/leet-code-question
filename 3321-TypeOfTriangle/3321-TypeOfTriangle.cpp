f26242523be16f7917b6f5beefe3b8ec9076544c
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 22:36:59 +0530

    Create 3321-TypeOfTriangle

---
+++ 3321-TypeOfTriangle/3321-TypeOfTriangle.cpp
@@ -0,0 +1,23 @@
+// Last updated: 5/3/2026, 10:36:59 PM
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if (a + b <= c || b + c <= a || c + a <= b) {
+            return "none";
+        }
+
+        if (a == b && b == c) {
+            return "equilateral";
+        }
+
+        if(a==b || b==c || c==a){
+            return "isosceles";
+        }
+
+        return "scalene";
+    }
+};
