09526966099118ef863a595249945168e8ae67b5
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 22:37:05 +0530

    Create 3312-NumberOfChangingKeys

---
+++ 3312-NumberOfChangingKeys/3312-NumberOfChangingKeys.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/3/2026, 10:37:04 PM
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int count = 0;
+        for (int i = 1; i < s.size(); i++) {
+            if (tolower(s[i]) != tolower(s[i - 1])) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
