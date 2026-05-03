34eac861196086ae3fee71baa375e7e057182e5e
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 17:15:24 +0530

    File updated at 5/3/2026, 5:15:24 PM

--- 3024-TypeofTriangle/3024-TypeofTriangle.cpp
+++ 3024-TypeofTriangle/3024-TypeofTriangle.cpp
@@ -1,23 +1,17 @@
-// Last updated: 5/3/2026, 4:46:37 PM
+// Last updated: 5/3/2026, 5:15:24 PM
 1class Solution {
 2public:
-3    string triangleType(vector<int>& nums) {
-4        int a = nums[0];
-5        int b = nums[1];
-6        int c = nums[2];
-7
-8        if (a + b <= c || b + c <= a || c + a <= b) {
-9            return "none";
-10        }
-11
-12        if (a == b && b == c) {
-13            return "equilateral";
-14        }
-15
-16        if(a==b || b==c || c==a){
-17            return "isosceles";
-18        }
-19
-20        return "scalene";
-21    }
-22};
+3    int mostWordsFound(vector<string>& sentences) {
+4        int maxword = 0;
+5
+6        for (string s : sentences) {
+7            int count = 1;
+8            for (char ch : s) {
+9                if(ch== ' ') count++;
+10            }
+11            maxword= max(maxword,count);
+12
+13        }
+14        return maxword;
+15    }
+16};
