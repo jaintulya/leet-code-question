96dc99c66f3d13d953e17b7b9594f69a79ced8c1
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 22:33:13 +0530

    File updated at 5/3/2026, 10:33:13 PM

--- 2535-DifferenceBetweenElementSumandDigitSumofanArray/2535-DifferenceBetweenElementSumandDigitSumofanArray.cpp
+++ 2535-DifferenceBetweenElementSumandDigitSumofanArray/2535-DifferenceBetweenElementSumandDigitSumofanArray.cpp
@@ -1,24 +1,14 @@
-// Last updated: 5/3/2026, 10:26:28 PM
+// Last updated: 5/3/2026, 10:33:13 PM
 1class Solution {
 2public:
-3    int differenceOfSum(vector<int>& nums) {
-4        int elementsum = 0;
-5        int digitsum = 0;
-6
-7        for (int a : nums) {
-8
-9            elementsum += a;
-10
-11            int temp = a;
-12
-13            while (temp > 0) {
-14
-15                digitsum += temp % 10;
-16
-17                temp = temp / 10;
-18            }
-19        }
-20
-21        return elementsum-digitsum;
-22    }
-23};
+3    int sumOfMultiples(int n) {
+4        int sum = 0;
+5        for (int i = 1; i <= n; i++) {
+6            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+7                sum += i; // sum=sum+i
+8            }
+9           
+10        }
+11         return sum;
+12    }
+13};
