7ee5458dc1b9ab069b8d6d1eed01d67a188de7ac
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-03 22:09:06 +0530

    File updated at 5/3/2026, 10:09:06 PM

--- 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.cpp
+++ 2108-FindFirstPalindromicStringintheArray/2108-FindFirstPalindromicStringintheArray.cpp
@@ -1,16 +1,19 @@
-// Last updated: 4/30/2026, 1:37:31 PM
+// Last updated: 5/3/2026, 10:09:06 PM
 1class Solution {
 2public:
 3    string firstPalindrome(vector<string>& words) {
-4        for(string &s:words){
-5            int i=0,j=s.size()-1;
-6            while(i<j && s[i]==s[j]){
-7                i++;
-8                j--;
-9            }
-10            if(i>=j)return s;
-11        
-12        }
-13        return "";
-14    }
-15};
+4        for (string s : words) {
+5            int i = 0; //abc //ada //racecar
+6            int j = s.size()-1; //5
+7
+8            while (i<j && s[i]==s[j]){
+9                i++; //3
+10                j--;//3
+11            }
+12
+13            if(i>=j) return s;
+14        }
+15
+16        return "";
+17    }
+18};
