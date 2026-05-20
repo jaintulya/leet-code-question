125d0fd772282eaef7d4a127b3716b20470f6f72
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-20 11:35:46 +0530

    Create 1347-MinimumNumberofStepstoMakeTwoStringsAnagram

---
+++ 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/version/cpp/1347-MinimumNumberofStepstoMakeTwoStringsAnagram_20260520_113544.cpp
@@ -0,0 +1,27 @@
+// Last updated: 5/20/2026, 11:35:44 AM
+1class Solution {
+2public:
+3    int sumOddLengthSubarrays(vector<int>& arr) {
+4
+5        int total = 0;
+6
+7        for(int i = 0; i < arr.size(); i++) {
+8
+9            int sum = 0;
+10
+11            for(int j = i; j < arr.size(); j++) {
+12
+13                sum += arr[j];
+14
+15                int length = j - i + 1;
+16
+17                if(length % 2 != 0) {
+18
+19                    total += sum;
+20                }
+21            }
+22        }
+23
+24        return total;
+25    }
+26};
