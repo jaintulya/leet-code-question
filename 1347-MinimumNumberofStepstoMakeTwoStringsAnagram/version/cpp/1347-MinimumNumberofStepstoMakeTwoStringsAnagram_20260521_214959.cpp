0bf8c96ab03b6d2c6af84e7c46c08987f7747c32
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-21 21:50:01 +0530

    Create 1347-MinimumNumberofStepstoMakeTwoStringsAnagram

---
+++ 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/version/cpp/1347-MinimumNumberofStepstoMakeTwoStringsAnagram_20260521_214959.cpp
@@ -0,0 +1,33 @@
+// Last updated: 5/21/2026, 9:49:59 PM
+1class Solution {
+2public:
+3    int maxFreqSum(string s) {
+4
+5        vector<int> freq(26, 0);
+6
+7        for(char ch : s) {
+8            freq[ch - 'a']++;
+9        }
+10
+11        int vowelMax = 0;
+12        int consoMax = 0;
+13
+14        for(int i = 0; i < 26; i++) {
+15
+16            char ch = i + 'a';
+17
+18            if(ch == 'a' || ch == 'e' || ch == 'i' ||
+19               ch == 'o' || ch == 'u') {
+20
+21                vowelMax = max(vowelMax, freq[i]);
+22            }
+23
+24            else {
+25
+26                consoMax = max(consoMax, freq[i]);
+27            }
+28        }
+29
+30        return vowelMax + consoMax;
+31    }
+32};
