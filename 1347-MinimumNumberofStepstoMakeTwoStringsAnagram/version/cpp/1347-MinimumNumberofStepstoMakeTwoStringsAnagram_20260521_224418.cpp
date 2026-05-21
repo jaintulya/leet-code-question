60365f74ce2a843699afaecf09aef0e14b8d5695
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-21 22:44:19 +0530

    Create 1347-MinimumNumberofStepstoMakeTwoStringsAnagram

---
+++ 1347-MinimumNumberofStepstoMakeTwoStringsAnagram/version/cpp/1347-MinimumNumberofStepstoMakeTwoStringsAnagram_20260521_224418.cpp
@@ -0,0 +1,20 @@
+// Last updated: 5/21/2026, 10:44:18 PM
+1class Solution {
+2public:
+3    vector<int> getSneakyNumbers(vector<int>& nums) {
+4     
+5
+6        vector<int>freq(nums.size(),0);
+7        vector<int> ans;
+8
+9        for(int i=0 ; i<nums.size();i++){
+10            freq[nums[i]]++;
+11        }
+12
+13        for(int i=0 ; i<nums.size();i++){
+14            if(freq[i]>1) ans.push_back(i);
+15        }
+16        return ans;
+17    } 
+18};
+19 
