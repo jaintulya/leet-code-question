dd69c52bd39e505e4e068e0dbc63e0e40aecc4ab
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-28 15:37:02 +0530

    Create 1281-SubtracttheProductandSumofDigitsofanInteger

---
+++ 1281-SubtracttheProductandSumofDigitsofanInteger/version/cpp/1281-SubtracttheProductandSumofDigitsofanInteger_20260528_153702.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/28/2026, 3:37:02 PM
+1class Solution {
+2public:
+3    int maximumWealth(vector<vector<int>>& accounts) {
+4        int maxWealth = 0;
+5
+6        for (int i = 0; i < accounts.size(); i++) {
+7            int sum = 0;
+8
+9            for (int j = 0; j < accounts[i].size(); j++) {
+10                sum+=accounts[i][j];
+11            }
+12
+13            maxWealth=max(maxWealth,sum);
+14        }
+15        return maxWealth;
+16    }
+17};
