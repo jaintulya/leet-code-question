0038c943db3ca7f1f30c009ceb57bbc584b3b882
Tulya Jain <tulya.jain.cg@gmail.com>
2026-05-28 14:37:16 +0530

    Create 387-FirstUniqueCharacterinaString

---
+++ 387-FirstUniqueCharacterinaString/version/cpp/387-FirstUniqueCharacterinaString_20260528_143716.cpp
@@ -0,0 +1,18 @@
+// Last updated: 5/28/2026, 2:37:16 PM
+1class Solution {
+2public:
+3    int firstUniqChar(string s) {
+4      unordered_map<char,int>mp;
+5
+6      for(int i=0; i<s.size();i++){
+7        mp[s[i]]++;
+8      }
+9
+10      for(int j=0 ; j<s.size();j++){
+11        if(mp[s[j]]==1){
+12            return j;
+13        }
+14      }
+15      return -1;
+16    }
+17};
