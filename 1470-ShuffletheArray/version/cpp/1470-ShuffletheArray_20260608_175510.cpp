5eaec6c86836d96135eb9e473d449d7a3af88432
Tulya Jain <tulya.jain.cg@gmail.com>
2026-06-08 17:55:09 +0530

    Create 1470-ShuffletheArray

---
+++ 1470-ShuffletheArray/version/cpp/1470-ShuffletheArray_20260608_175510.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/8/2026, 5:55:10 PM
+1class Solution {
+2public:
+3    vector<int> shuffle(vector<int>& nums, int n) {
+4
+5        vector<int> ans;
+6
+7        for(int i = 0; i < n; i++) {
+8
+9            ans.push_back(nums[i]);
+10            ans.push_back(nums[i + n]);
+11        }
+12
+13        return ans;
+14    }
+15};
