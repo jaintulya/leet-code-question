c3615fd6ed82d66aee80e20308511a83a34840fa
Tulya Jain <tulya.jain.cg@gmail.com>
2026-06-16 16:27:42 +0530

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/version/cpp/71-SimplifyPath_20260616_162742.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/16/2026, 4:27:42 PM
+1class Solution {
+2public:
+3    string simplifyPath(string path) {
+4        vector<string> st;
+5        string cur = "";
+6        int i = 0;
+7
+8        while (i <= path.size()) {
+9            if (i == path.size() || path[i] == '/') {
+10                if (cur == "..") {
+11                    if (!st.empty()) st.pop_back();
+12                }
+13                else if (!cur.empty() && cur != ".") {
+14                    st.push_back(cur);
+15                }
+16                cur = "";
+17            } else {
+18                cur += path[i];
+19            }
+20            i++;
+21        }
+22
+23        string ans = "";
+24        for (string &dir : st) {
+25            ans += "/" + dir;
+26        }
+27
+28        return ans.empty() ? "/" : ans;
+29    }
+30};
