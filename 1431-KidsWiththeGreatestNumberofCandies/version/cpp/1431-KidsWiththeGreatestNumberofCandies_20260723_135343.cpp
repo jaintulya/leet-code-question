f1f9eca307a25d06f1d70a0c89be0058e3e3620f
Tulya Jain <tulya.jain.cg@gmail.com>
2026-07-23 13:53:43 +0530

    Create 1431-KidsWiththeGreatestNumberofCandies

---
+++ 1431-KidsWiththeGreatestNumberofCandies/version/cpp/1431-KidsWiththeGreatestNumberofCandies_20260723_135343.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/23/2026, 1:53:43 PM
+1class Solution {
+2public:
+3    bool isMonotonic(vector<int>& nums) {
+4        int inc=0;
+5        int dec=0;
+6        for(int i=0;i<nums.size()-1;i++){
+7            if(nums[i]<=nums[i+1]) inc++;
+8              if(nums[i+1]<=nums[i]) dec++;
+9        }
+10
+11        if(inc==nums.size()-1||dec==nums.size()-1){
+12            return true;
+13           
+14        } else return false;
+15    }
+16};
