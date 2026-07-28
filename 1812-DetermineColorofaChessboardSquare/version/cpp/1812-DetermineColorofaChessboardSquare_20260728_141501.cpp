7aa61172bed3205fb4a64782dd322ab47e4b6196
Tulya Jain <tulya.jain.cg@gmail.com>
2026-07-28 14:15:01 +0530

    Create 1812-DetermineColorofaChessboardSquare

---
+++ 1812-DetermineColorofaChessboardSquare/version/cpp/1812-DetermineColorofaChessboardSquare_20260728_141501.cpp
@@ -0,0 +1,9 @@
+// Last updated: 7/28/2026, 2:15:01 PM
+1class Solution {
+2public:
+3    int finalElement(vector<int>& nums) {
+4     int ans;
+5     ans=max(nums.front(),nums.back());
+6     return ans;
+7    }
+8};
