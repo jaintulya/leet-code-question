5413985ae038a9fa07a557892d4e5eec2c2209a0
Tulya Jain <tulya.jain.cg@gmail.com>
2026-08-18 16:55:13 +0530

    Create 3894-TrafficSignalColor

---
+++ 3894-TrafficSignalColor/version/cpp/3894-TrafficSignalColor_20260818_165513.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/18/2026, 4:55:13 PM
+1class Solution {
+2public:
+3    string trafficSignal(int timer) {
+4        if (timer == 0) {
+5            return "Green";
+6        } else if (timer == 30) {
+7            return "Orange";
+8        } else if (timer > 30 && timer <= 90) {
+9            return "Red";
+10        }
+11          return "Invalid";
+12    }
+13  
+14
+15}
+16;
